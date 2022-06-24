3e4533d2d432859f408b1c1935d46ae9d78d7cbd
Not-a-whale <49877969+Not-a-whale@users.noreply.github.com>
2022-06-24 23:38:53 +0200

    completed

---
+++ HW_12/Adder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_12
+{
+    internal class Adder
+    {
+        public delegate void ResultHandler(int smth);
+        public delegate void Summator(int a, int b);
+        public static event ResultHandler? ShowResult;
+
+        public static void ShowSum1(int sum) => Console.WriteLine($"The sum is {sum} #1");
+        public static void ShowSum2(int sum) => Console.WriteLine($"The sum is {sum} #2");
+        public static void GetResult1(int a, int b)
+        {
+            int c = a + b;
+            ShowResult = ShowSum1;
+            ShowResult?.Invoke(c);
+        }
+
+        public static void GetResult2(int a, int b)
+        {
+            int c = a + b;
+            ShowResult = ShowSum2;
+            ShowResult?.Invoke(c);
+        }
+
+        public static void trySum(Summator sm,int a,int b)
+        {
+            try
+            {
+                sm(a, b);
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ HW_12/Program.cs
@@ -0,0 +1,14 @@
+﻿using HW_12;
+using static HW_12.Adder;
+
+internal class Program
+{
+    private static void Main(string?[] args)
+    {
+        Summator sumDeleg1 = Adder.GetResult1;
+        Summator sumDeleg2 = Adder.GetResult2;
+
+        Adder.trySum(sumDeleg1, 5, 10);
+        Adder.trySum(sumDeleg2, 10, 20);
+    }
+}
